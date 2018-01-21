c9d69f027d683f32627eb735a90ed0c8121ce69a
NARVIS-UECK\ueck <ulrich.eck@tum.de>
2018-01-21 12:39:31 +0100

    use svn to also get externals (cmake modules) - build on win vs2017

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class H3DUtilConan(ConanFile):
     name = "h3dutil"
-    version = "1.3"
+    version = "1.4-beta"
     url = "https://github.com/ulricheck/conan-h3dutil.git"
 
     short_paths = True
@@ -21,31 +21,31 @@ class H3DUtilConan(ConanFile):
             self.requires("h3dapi_windows_dependencies/[>=2.3]@camposs/stable")
 
     def source(self):
-        repo_url = "https://www.h3dapi.org:8090/H3DUtil/branches/release%s/ source" % self.version
-        self.run("git svn clone %s source" % repo_url)
+        repo_url = "https://www.h3dapi.org:8090/H3DUtil/trunk/H3DUtil/"
+        self.run("svn checkout %s source" % repo_url)
        
     def build(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        # fix a compiler issue with pthread on vs 2015+
+#         if self.settings.os == "Windows":
+#             vs_version = int(str(self.settings.compiler.version))
+#             if vs_version >=14:
+#                 tools.replace_in_file("source/build/CMakeLists.txt", "INCLUDE_DIRECTORIES( ${PTHREAD_INCLUDE_DIR} )", '''INCLUDE_DIRECTORIES( ${PTHREAD_INCLUDE_DIR} )
+# add_definitions(-DHAVE_STRUCT_TIMESPEC)''')
+
         cmake.configure(source_dir=os.path.join("source", "build"))
         cmake.build()
         cmake.install()
 
     def package(self):
-        self.copy(pattern='*.h' , dst="include", src="package/include", keep_path=False)
-        self.copy(pattern='*.cmake' , dst="cmake", src="package/cmake", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src="package/lib", keep_path=False)
-        self.copy(pattern="*.dll", dst="bin", src="package/lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", src="package/lib", keep_path=False)
-        self.copy(pattern="*.dylib*", dst="lib", src="package/lib", keep_path=False)  
-
+        self.copy(pattern='*.h' , dst="include", src="source/include", keep_path=False)
+        self.copy(pattern='*.cmake' , dst="cmake", src="source/cmake", keep_path=False)
 
     def package_info(self):
-        # self.cpp_info.defines.append("HAVE_H3DUTIL")
-        h3d_modules = ["h3dutil"]
+        if self.settings.arch == "x86":
+            libfolder = "lib32"
+        else:
+            libfolder = "lib64"
 
-        suffix = ""
-        if self.settings.build_type == "Debug":
-            suffix = "d"
-        for lib in h3d_modules:
-            self.cpp_info.libs.append("%s%s" % (lib, suffix))
+        self.cpp_info.libs = tools.collect_libs(self, folder=libfolder)
