7ad52c999e69b341cc836b377e1df7b644e00109
Ulrich Eck <ueck@net-labs.de>
2018-01-26 22:35:06 +0100

    compile on osx

--- conanfile.py
+++ conanfile.py
@@ -19,6 +19,8 @@ class H3DUtilConan(ConanFile):
     def requirements(self):
         if self.settings.os == "Windows":
             self.requires("h3dapi_windows_dependencies/[>=2.3]@camposs/stable")
+        else:
+            self.requires("libiconv/[>=1.15]@bincrafters/stable")
 
     def source(self):
         repo_url = "https://www.h3dapi.org:8090/H3DUtil/trunk/H3DUtil/"
@@ -27,12 +29,8 @@ class H3DUtilConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        # fix a compiler issue with pthread on vs 2015+
-#         if self.settings.os == "Windows":
-#             vs_version = int(str(self.settings.compiler.version))
-#             if vs_version >=14:
-#                 tools.replace_in_file("source/build/CMakeLists.txt", "INCLUDE_DIRECTORIES( ${PTHREAD_INCLUDE_DIR} )", '''INCLUDE_DIRECTORIES( ${PTHREAD_INCLUDE_DIR} )
-# add_definitions(-DHAVE_STRUCT_TIMESPEC)''')
+        if self.settings.os == "Macos":
+            cmake.definitions["CMAKE_SHARED_LINKER_FLAGS"] = "-liconv"
 
         cmake.configure(source_dir=os.path.join("source", "build"))
         cmake.build()
@@ -42,9 +40,11 @@ class H3DUtilConan(ConanFile):
         self.copy(pattern='*.h' , dst="include", src="source/include", keep_path=False)
 
     def package_info(self):
-        if self.settings.arch == "x86":
-            libfolder = "lib32"
-        else:
-            libfolder = "lib64"
+        libfolder = 'lib'
+        if self.settings.os == "Windows":
+            if self.settings.arch == "x86":
+                libfolder = "lib32"
+            else:
+                libfolder = "lib64"
 
         self.cpp_info.libs = tools.collect_libs(self, folder=libfolder)
